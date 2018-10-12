4f537efeeb882464c4d42be00df97a558fbccdf4
zemzale <14844365+zemzale@users.noreply.github.com>
2018-10-12 09:10:06 +0300

    Add files via upload

--- task-1/main.cpp
+++ task-1/main.cpp
@@ -1,6 +1,6 @@
 /*
  * Alberts Zemzāle, az18071
- * A32 Atrast divu naturāļu skaitļu m un n mazāko kopīgo dalāmo
+ * A32 Atrast divu naturālu skaitļu m un n mazāko kopīgo dalāmo
  * Programma izveidota 13/09/2018
  */
 #include <iostream>
@@ -13,14 +13,14 @@ using namespace std;
  * Finds the greatest common diveders between two natural numbers.
  * This WILL NOT work for numbers that are lower then 1
  *
- * @param int num1 First number 
+ * @param int num1 First number
  * @param int num2 Second number
  *
- * @return int Greatest common divisor 
+ * @return int Greatest common divisor
  */
 int findGreatestCommonDivisor(int num1, int num2) {
 
-    //Set the divisable as the larger number and divider as the smaller 
+    //Set the divisable as the larger number and divider as the smaller
     int gcdDivider = num1 < num2 ? num1 : num2;
     int gcdDivisable = num1 > num2 ? num1 : num2;
 
@@ -28,7 +28,7 @@ int findGreatestCommonDivisor(int num1, int num2) {
     int gcd = 0;
 
     /*
-     * Divide numbers untill the reminder is 0. 
+     * Divide numbers untill the reminder is 0.
      * The next divider is the reminder of the last division.
      * The next divisable is the diveder of the last division.
      */
@@ -49,7 +49,7 @@ int findGreatestCommonDivisor(int num1, int num2) {
 /*
  * Find the lowest common multiple of two numbers
  *
- * Find the lower common multiple of two integer numbers 
+ * Find the lower common multiple of two integer numbers
  * that are bigger then 0.
  *
  * @param int num1 First number
@@ -62,30 +62,34 @@ int findLowestCommonMultiple(int num1, int num2) {
     int gcd = findGreatestCommonDivisor(num1, num2);
     int leastCommonMultiple = num1 * num2 / gcd;
 
-    return leastCommonMultiple; 
+    return leastCommonMultiple;
 }
 
 /*
  * Take natural number input with a promt message
  *
- * Take natural number user input from stdin with a simple string promt 
+ * Take natural number user input from stdin with a simple string promt
  * message. The function will core dump on non integer input
  * IS RECURSIVE will return only when the input is right
  *
- * @param string msg Message to promt user with when inputing 
+ * @param string msg Message to promt user with when inputing
  *
  * @return Integer value from user
  */
 int takeNaturalNumberInput(string msg) {
     int inputNumber = 0;
-    cout << msg << " : ";
-    cin >> inputNumber;
+    bool correctInput = false;
 
-    //Check if user input is correct. If not call the function again
-    if (inputNumber < 1) {
-        cout << "Skaitlim jābūt naturālam" << endl;
-        inputNumber = takeNaturalNumberInput(msg);
-    }
+    //Ask for user input until the number inputed is natural
+    do{
+        cout << msg << " : ";
+        cin >> inputNumber;
+        if (inputNumber < 0) {
+            cout << "Number must be natural" << endl;
+        } else {
+            correctInput = true;
+        }
+    }while(!correctInput);
 
     return inputNumber;
 
@@ -93,25 +97,33 @@ int takeNaturalNumberInput(string msg) {
 
 int main() {
 
-    int userAnswer = 0; 
+    int userAnswer = 0;
 
     /*
      * Take user input
      * compute the result
-     * repeat untill user wants to exit
+     * repeat until user wants to exit
      */
     do {
-        int number1 = takeNaturalNumberInput("Ievadiet pirmo skaitli");
-        int number2 = takeNaturalNumberInput("Ieavdiet otro  skaitli");
-        int answer = findLowestCommonMultiple(number1, number2);
-        cout << "Mazākais kopīgais dalāmais ir : " << answer << endl;
+        int number1 = takeNaturalNumberInput("Input first number");
+        int number2 = takeNaturalNumberInput("Input second number");
+        int answer = 0;
+        if (number1 == 0 || number2 == 0) {
+            answer = 0;
+        } else {
+            answer = findLowestCommonMultiple(number1, number2);
+        }
 
-        cout << "Ja vēlaties turpināt(0) vai beigt(1) programmas darbību : ";
+        cout << "Least common multiple : " << answer << endl;
+
+        cout << "Continue(0) or exit(1) : ";
         cin >> userAnswer;
-        
+
     } while (userAnswer == 0);
 
     return 0;
 }
+
+
 
 
