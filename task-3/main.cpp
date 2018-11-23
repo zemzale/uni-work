e5f83e55128240b29bd8f85cfa39d5068a31c1ca
zemzale <14844365+zemzale@users.noreply.github.com>
2018-11-23 09:49:28 +0200

    Add files via upload

--- task-3/main.cpp
+++ task-3/main.cpp
@@ -1,6 +1,6 @@
 /*
  * Author : Alberts Zemzale AZ18071
- * Doti divi veselu skaitÄ¼u masÄ«vi A(m) un B(n). Atrast lielÄko tÄdu elementu masÄ«vÄ A, kurÅ¡ nav atrodams masÄ«vÄ B.
+ * Doti divi veselu skaitïu masîvi A(m) un B(n). Atrast lielâko tâdu elementu masîvâ A, kurğ nav atrodams masîvâ B.
  */
 
 #include <iostream>
@@ -8,29 +8,93 @@
 #include <iterator>
 
 
+//Return a pointer for an array from user input
+int* getUserArray(int arraySize) {
+	int* array = new int[arraySize];
+	for (int i = 0; i < arraySize; i++) {
+		int number = 0;
+		std::cout << "Input number for array  : ";
+		std::cin >> number;
+		
+		array[i] = number;
+	}
+	return array;
+}
+
+//Print out the biggest number from array 1 that isn't in array 2
+void getBiggestDiffNumber(int* pArray1, int* pArray2, int arraySize) {
+	
+	int diffNumber = 0;
+	bool diffFound = false;
+
+	//Iterate over array 1 and compare the numbers with conentns of array 2
+	for(int i = 0; i < arraySize; i++) {
+		bool arrayContains = false;
+		int j = 0;
+		while(j < arraySize) {
+			//compare the values, if it contains the values then we exit out of the loop
+			if (pArray1[i] == pArray2[j]) {
+				arrayContains = true;	
+				j = arraySize;
+			}
+			j++;
+		}
+		//check if need to change diff number
+		if (!arrayContains && (!diffFound || diffNumber < pArray1[i])) {
+			diffFound = true;
+			diffNumber = pArray1[i];
+		}
+	}
+	
+	//Print out final values
+	if (diffFound) {
+		std::cout << "Biggest number in array 1 that is not in array 2 : " << diffNumber << std::endl;			
+	} else {
+		std::cout << "There is no such a number that is in array 1 that isn't in array 2" << std::endl;
+	}
+}
 
 int main() {
+	
+	int continueExecution = 0;
+	
+	do {
+		continueExecution = 0;
+		int arraySize = 0;
+		
+		std::cout << "What is the size of the array you want to make?";
+		std::cin >> arraySize;
+		
+		//Check if array size is correct
+		if (arraySize > 0) {
+			
+			//Get arrays
+			std::cout << "Fill array 1" << std::endl;
+			int* pArray1 = getUserArray(arraySize);
+			std::cout << "Fill array 2" << std::endl;
+			int* pArray2 = getUserArray(arraySize);
+			
+			//Execute programm logic
+			getBiggestDiffNumber(pArray1, pArray2, arraySize);
+			
+			//Clean up
+			delete pArray1;
+			delete pArray2;
+			
+		} else {
+			std::cout << "The array needs to be bigger then 0!" << std::endl;
+			continueExecution = 1;
+			continue;
+		}
+		
+		
+		std::cout << "Do you want to continue (1) or exit (0) ?" << std::endl;
+		std::cin >> continueExecution;
+	}while (continueExecution);
+	
 
-    std::array<int, 7> array1 = {1, 2, 3, 23, 45, 23432, 1};
-    std::array<int, 7> array2 = {0, 2, 3, 4, 23, 1, 45};
-    int diffNumber = 0;
-    
 
-    for(auto it = array1.begin(); it != array1.end(); ++it) {
-        bool arrayContains = false;
-        auto ij = array2.begin();
-        while(ij != array2.end()) {
-            if (*ij == *it) {
-                arrayContains = true;
-            }
-            ij++;
-        }
-        if (diffNumber < *it && !arrayContains) {
-            diffNumber = *it;
-        }
-    }
 
-    std::cout << diffNumber << std::endl;
 
     return 0;
 }
